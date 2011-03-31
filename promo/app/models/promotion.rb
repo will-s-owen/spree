08e36ebab52f955a7b1754e35fc73113ccd32b54
David North <davidnorth@railsdog.com>
2011-03-31 14:51:21 +0100

    Preferences for removed promotion attributes

--- promo/app/models/promotion.rb
+++ promo/app/models/promotion.rb
@@ -3,7 +3,21 @@ class Promotion < Activator
   calculated_adjustments
   alias credits promotion_credits
 
-  has_many :promotion_rules, :autosave => true
+
+  MATCH_POLICIES = %w(all any)
+
+  preference :combine, :boolean, :default => false
+  preference :usage_limit, :integer
+  preference :match_policy, :string, :default => MATCH_POLICIES.first
+
+  [:combine, :usage_limit, :match_policy].each do |field|
+    alias_method field, "preferred_#{field}"
+    alias_method "#{field}=", "preferred_#{field}="
+  end
+
+
+
+  has_many :promotion_rules, :foreign_key => 'activator_id', :autosave => true
   accepts_nested_attributes_for :promotion_rules
   alias_method :rules, :promotion_rules
 
@@ -17,7 +31,6 @@ class Promotion < Activator
     end
   end
 
-  MATCH_POLICIES = %w(all any)
 
   def eligible?(order)
     !expired? && rules_are_eligible?(order)
