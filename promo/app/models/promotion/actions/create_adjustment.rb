45b9026d6af7291d2f04b24240b24122e1785537
David North <davidnorth@railsdog.com>
2011-04-13 23:16:30 +0100

    Fixed failures in create_adjustment_spec

--- promo/app/models/promotion/actions/create_adjustment.rb
+++ promo/app/models/promotion/actions/create_adjustment.rb
@@ -4,7 +4,7 @@ class Promotion::Actions::CreateAdjustment < PromotionAction
 
   before_create do |a|
     c = a.build_calculator
-    c.type = Promotion.calculators.first.to_s
+    c.type = Promotion::Actions::CreateAdjustment.calculators.first.to_s
   end
 
   def perform(options = {})
@@ -14,14 +14,6 @@ class Promotion::Actions::CreateAdjustment < PromotionAction
       amount = order.item_total if amount > order.item_total
       order.adjustments.promotion.reload.clear unless promotion.combine? and order.adjustments.promotion.all? { |credit| credit.originator.promotion.combine? }
       order.update!
-
-      # Adjustment.create!({
-      #     :label => "#{I18n.t(:promotion)} (#{promotion.code})",
-      #     :source => promotion,
-      #     :amount => -amount.abs,
-      #     :order => order
-      #   }).inspect
-
       create_adjustment("#{I18n.t(:promotion)} (#{promotion.code})", order, order)
     end
   end
--- promo/spec/models/promotion/actions/create_adjustment_spec.rb
+++ promo/spec/models/promotion/actions/create_adjustment_spec.rb
@@ -5,7 +5,7 @@ describe Promotion::Actions::CreateAdjustment do
 
   # From promotion spec:
   context "#perform" do
-    let(:order) { Order.new }
+    let(:order) { Factory(:order) }
     let(:promotion) { Promotion.new }
     let(:action) { Promotion::Actions::CreateAdjustment.new }
 
@@ -20,11 +20,11 @@ describe Promotion::Actions::CreateAdjustment do
       promotion.stub(:eligible? => false)
       order.stub(:promotion_credit_exists? => nil)
       action.perform(:order => order)
-      action.promotion_credits.should have(0).item
+      promotion.credits_count.should == 0
     end
 
     it "should create a discount when order is eligible" do
-      order.stub(:promotion_credit_exists? => nil)
+      order.stub(:promotion_credit_exists? => false)
       order.stub(:ship_total => 5, :item_total => 50, :reload => nil)
       promotion.stub(:eligible? => true)
       promotion.code = 'PROMO'
@@ -37,8 +37,8 @@ describe Promotion::Actions::CreateAdjustment do
         :order => order
       }
 
-      PromotionCredit.should_receive(:create!).with(attrs)
       action.perform(:order => order)
+      promotion.credits_count.should == 1
     end
 
   end
