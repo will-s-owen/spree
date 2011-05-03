be4f37497310cadc9cfa3630aa5304a23ccf62c5
David North <davidnorth@railsdog.com>
2011-05-03 18:19:38 +0100

    Simplify migrations for activators, fixing some failing specs

--- promo/db/migrate/20110331094351_promotion_changes_to_subclass_of_activator.rb
+++ promo/db/migrate/20110331094351_promotion_changes_to_subclass_of_activator.rb
@@ -1,37 +1,20 @@
 class PromotionChangesToSubclassOfActivator < ActiveRecord::Migration
   def self.up
-    drop_table :activators
-
-    rename_table :promotions, :activators
-
-    add_column :activators, :event_name, :string
-    add_column :activators, :type, :string
-
-    remove_column :activators, :code
-    remove_column :activators, :usage_limit
-    remove_column :activators, :combine
-    remove_column :activators, :match_policy
-
+    drop_table :promotions
     rename_column :promotion_rules, :promotion_id, :activator_id
   end
 
   def self.down
-    rename_table :activators, :promotions
-
-    create_table "activators", :force => true do |t|
-      t.integer "promotion_id"
-      t.string  "event_name"
+    create_table "promotions", :force => true do |t|
+      t.string   "name"
+      t.string   "code"
+      t.string   "description"
+      t.integer  "usage_limit"
+      t.boolean  "combine"
+      t.datetime "expires_at"
+      t.datetime "created_at"
+      t.datetime "updated_at"
+      t.datetime "starts_at"
     end
-
-    remove_column :promotions, :event_name
-    remove_column :promotions, :type
-
-
-    add_column :promotions, :code, :string
-    add_column :promotions, :usage_limit, :integer
-    add_column :promotions, :combine, :boolean
-    add_column :promotions, :match_policy
-
-    rename_column :promotion_rules, :activator_id, :promotion_id
   end
 end
