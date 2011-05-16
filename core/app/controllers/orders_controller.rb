f73b053be6c91617b66ea492c61fc1f5130adde6
David North <davidnorth@railsdog.com>
2011-05-16 17:42:43 +0100

    Fixed several spec and feature failures

--- core/app/controllers/orders_controller.rb
+++ core/app/controllers/orders_controller.rb
@@ -11,18 +11,9 @@ class OrdersController < Spree::BaseController
   def update
     @order = current_order
     if @order.update_attributes(params[:order])
-
-      if @order.coupon_code.present?
-        fire_event('spree.checkout.coupon_code_added', :coupon_code => @order.coupon_code)
-      end
-
       @order.line_items = @order.line_items.select {|li| li.quantity > 0 }
-<<<<<<< HEAD
-      respond_with(@order) { |format| format.html { redirect_to cart_path } }
-=======
       fire_event('spree.order.contents_changed')
-      redirect_to cart_path
->>>>>>> Added fire_event convenience method
+      respond_with(@order) { |format| format.html { redirect_to cart_path } }
     else
       respond_with(@order)
     end
